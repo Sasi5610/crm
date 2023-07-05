679b6fd0561ae995baca3e76f6de1024a09a45b0
Sasi5610 <sasikumarv5610@gmail.com>
2023-07-05 20:41:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import App from "./App";
+
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  config.headers["x-access-token"] = token;
+
+  return config;
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <ToastContainer />
+    <App />
+  </BrowserRouter>
+);
